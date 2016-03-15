758b3128b0b2e61aebaa86ec1cd8a16113048a74
Jack Grossmann <jtg028@gmail.com>
2016-03-14 22:42:52 -0400

    more configs

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,13 @@ var connect = require('gulp-connect')
 var browserify = require('browserify')
 var source = require('vinyl-source-stream')
 var ngAnnotate = require('gulp-ng-annotate');
+var rename = require("gulp-rename");
+var streamify = require('gulp-streamify');
+
+// configure paths for browserify
+var cwd = process.cwd();
+process.env.NODE_PATH = process.env.NODE_PATH || cwd + '/node_modules';
+process.env.NODE_PATH += ":"+cwd+"/app";
 
 gulp.task('connect', function() {
   connect.server({
@@ -12,13 +19,19 @@ gulp.task('connect', function() {
   })
 })
 
+gulp.task('vendor', function() {
+    return browserify('./app/vendor.js')
+    .bundle()
+    .pipe(source('vendor.js'))
+    .pipe(gulp.dest('./public/js/'));
+  })
+
 gulp.task('browserify', function() {
-    // Grabs the app.js file
+      // Grabs the app.js file
     return browserify('./app/app.js')
     .bundle()
     .pipe(source('app.js'))
-    .pipe(ngAnnotate())
-    // saves it the public/js/ directory
+    .pipe(streamify(ngAnnotate()))
     .pipe(gulp.dest('./public/js/'));
   })
 
@@ -33,9 +46,9 @@ gulp.task('copy', function(){
 })
 
 gulp.task('watch', function() {
-  gulp.watch('app/**/*.js', ['browserify']);
+  gulp.watch('app/**/*.js', ['browserify', 'copy']);
   gulp.watch('app/**/*.html', ['copy']);
   gulp.watch('sass/style.sass', ['sass']);
 })
 
-gulp.task('default', ['connect', 'copy', 'watch'])
+gulp.task('default', ['connect', 'copy', 'vendor', 'watch'])
