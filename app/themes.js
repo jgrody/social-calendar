b22c792d30d825e942defd2e37bda219ec4c1a1c
Jack Grossmann <jtg028@gmail.com>
2016-04-13 20:22:23 -0400

    Theme module

---
+++ app/themes.js
@@ -0,0 +1,21 @@
+module.exports = function($mdThemingProvider){
+  "ngInject";
+
+  var customBlueMap = $mdThemingProvider
+    .extendPalette('light-blue', {
+      'contrastDefaultColor': 'light',
+      'contrastDarkColors': ['50'],
+      '50': 'ffffff'
+    });
+
+  $mdThemingProvider.definePalette('customBlue', customBlueMap);
+
+  $mdThemingProvider
+  .theme('default')
+    .primaryPalette('customBlue', {
+      'default': '500',
+      'hue-1': '50'
+    }).accentPalette('pink');
+
+  $mdThemingProvider.theme('input', 'default').primaryPalette('grey')
+}
